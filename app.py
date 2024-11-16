e911966806fbddc959216de7e53cb80e5f409d20
Lucas <lucasmoreirar6@gmail.com>
2024-11-15 21:06:11 -0300

    Adicao do arquivo de relatorios

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def main():
         st.sidebar.page_link("pages/clients_overview.py", label="Clientes", icon=":material/person:")
         st.sidebar.page_link("pages/inv_overview.py", label="Estoque", icon=":material/inventory:")
         st.sidebar.page_link("pages/order_overview.py", label="Pedidos", icon=":material/orders:")
+        st.sidebar.page_link("pages/reports_overview.py", label="Relatorios", icon=":material/lab_profile:")
         
         st.sidebar.markdown("---")
         
--- pages/reports_overview.py
+++ pages/reports_overview.py
@@ -1,13 +1,12 @@
-import streamlit as st #type: ignore
+import streamlit as st#type: ignore
 
 st.set_page_config(layout="wide")
 
 st.sidebar.title("Navegação")
 st.sidebar.page_link("pages/clients_overview.py", label="Clientes", icon=":material/person:")
 st.sidebar.page_link("pages/inv_overview.py", label="Estoque", icon=":material/inventory:")
-st.sidebar.page_link("pages/reports_overview.py", label="Relatorios", icon=":material/lab_profile:")
+st.sidebar.page_link("pages/order_overview.py", label="Pedidos", icon=":material/orders:")
 st.sidebar.page_link("app.py", label="Voltar para inicio", icon=":material/home:")
 
 
-st.title("Pedidos")
-
+st.title("Relatorios")  
